b7aff57229be1b51ab161062ffd36badea7e8082
esritines <esrit.ines@live.com.mx>
2015-04-23 11:16:26 -0500

    se agrego el metodo para traer la pregunta

--- src/Controladores/Datos.java
+++ src/Controladores/Datos.java
@@ -4,18 +4,24 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
 public class Datos {
 
     private static String usuario;
-    private static int puntos;
+    private static ArrayList<Integer> preguntas = new ArrayList<>();
+    private static Iterator<Integer> recorrer;
 
     public static String getUsuario() {
         return usuario;
     }
 
+    public void vaciarPreguntas(){
+        preguntas.clear();
+    }
+    
     public boolean traerDatos(String usuario) {
 
         try {
@@ -77,12 +83,48 @@ public class Datos {
                 usuarios.add(consulta.getString(1));
                 puntos.add(consulta.getInt(2));
             }
-            
+
             consulta.close();
             traerRegistros.close();
 
         } catch (SQLException ex) {
             Logger.getLogger(Datos.class.getName()).log(Level.SEVERE, null, ex);
         }
+    }
+
+    public void traerPregunta(String preguntaTemp, ArrayList respuestasTemp) {
+
+        recorrer = preguntas.iterator();
+        
+        boolean valor;
+
+        do {
+            valor = false;
+            int n = (int) (Math.random() * 2) + 1;
+
+            try {
+                Statement consultarDatos = ConexionBD.conexion.createStatement();
+                ResultSet consulta = consultarDatos.executeQuery("select pregunta, respuesta from preguntas where id = '" + n + "'");
+              
+                while (recorrer.hasNext()) {
+                    if (n == recorrer.next()) {
+                        valor = true;
+                        break;
+                    }
+                }
+
+                if (!valor) {
+                    while (consulta.next()) {
+                        preguntaTemp = consulta.getString(1);
+                        respuestasTemp.add(consulta.getString(2));
+                    }
+                    preguntas.add(n);
+                }
+
+            } catch (SQLException ex) {
+                Logger.getLogger(Datos.class.getName()).log(Level.SEVERE, null, ex);
+            }
+        } while (valor);
+
     }
 }
