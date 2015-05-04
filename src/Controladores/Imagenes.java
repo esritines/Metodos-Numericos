db9bc423d39a614279db793e8cff31fad04df0c2
esritines <esrit.ines@live.com.mx>
2015-05-04 13:13:36 -0500

    se modifico la ruta, ya que estaba mal

--- src/Controladores/Imagenes.java
+++ src/Controladores/Imagenes.java
@@ -3,11 +3,10 @@ package Controladores;
 import javax.swing.ImageIcon;
 
 public class Imagenes {
-
     public static final ImageIcon si = new ImageIcon("./src/Diseño/si.png");
     public static final ImageIcon no = new ImageIcon("./src/Diseño/no.png");
     public static final ImageIcon usuarios = new ImageIcon("./src/Diseño/usuario.png");
     public static final ImageIcon atras = new ImageIcon("./src/Diseño/atras.png");
-    public static final ImageIcon preguntas = new ImageIcon("./src/Diseño/atras.png");
+    public static final ImageIcon preguntas = new ImageIcon("./src/Diseño/pregunta.png");
     public static final ImageIcon fondo = new ImageIcon("./src/Diseño/fondo.png");
 }
